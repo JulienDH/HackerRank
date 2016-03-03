520691b462799049a87a2951e74eeab38bc663f1
JulienDH <j.dhainaut@live.com>
2016-03-03 18:15:18 +0100

    try and catch structure

--- javascript/tryCatchFinally.js
+++ javascript/tryCatchFinally.js
@@ -1,4 +1,6 @@
 //Try and catch structure
+
+//Example 1
 //How to reverse a string variable
 
 var my_string = "abcd"
@@ -12,4 +14,28 @@ catch (error){
 }
 finally {
    console.log("Type of my_string is : " + (typeof my_string))
+}
+
+
+//Example 2
+//How to evaluate a number
+try {
+    if (my_height/2 === NaN){
+        throw new Error("notANumberError");
+    }
+    if (my_height > 10){
+        throw new Error("hugeHeightError");
+    }
+    if (my_height < 4){
+        throw new Error("tinyHeightError");
+    }
+    console.log(my_height)
+}
+catch (error){
+    if (error.message == "notANumberError")
+        console.log(error.message)
+    if (error.message == "hugeHeightError")
+        console.log(error.message)
+    if (error.message == "tinyHeightError")
+        console.log(error.message)
 }
