cc52a534489760d4cc364a82e53902648171c019
JulienDH <j.dhainaut@live.com>
2016-03-03 18:14:27 +0100

    my first constructor

---
+++ javascript/constructor.js
@@ -0,0 +1,9 @@
+//My first js constructor
+
+var Car = function(car_name, car_mileage, car_max_speed){
+
+    this.name = car_name;
+    this.mileage = car_mileage;
+    this.max_speed = car_max_speed;
+
+};
